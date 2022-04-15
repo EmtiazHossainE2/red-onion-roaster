696b80f29ba2fee06127fa8fbf7d8ae20059d998
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-15 20:25:56 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound';
+import Home from './pages/Home/Home/Home';
+import Cart from './pages/Shared/Cart/Cart';
+import Login from './pages/Account/Login/Login';
+import Signup from './pages/Account/Signup/Signup';
+import Navbar from './pages/Shared/Navbar/Navbar';
 
 const App = () => {
     return (
         <div>
-            <h2 className='text-center'>Red Onion</h2>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}></Route>
+                <Route path='/cart' element={<Cart/>}></Route>
+                <Route path='/login' element={<Login/>}></Route>
+                <Route path='/signup' element={<Signup/>}></Route>
+                <Route path='*' element={<NotFound/>}></Route>
+            </Routes>
         </div>
     );
 };

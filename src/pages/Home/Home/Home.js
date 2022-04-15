9a2759456fbd44a41df8f49e04c6348db44a78ff
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-16 04:28:11 +0600

    navigate to foodlist ,btn added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Banner from '../Banner/Banner';
 import Food from '../Food/Food/Food';
 
@@ -7,6 +8,12 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Food></Food>
+            <div className='text-center'>
+                <Link to='/foodlist'>
+                <button className='btn btn-danger fw-bold'>Checkout your food</button>
+                </Link>
+                
+            </div>
         </div>
     );
 };

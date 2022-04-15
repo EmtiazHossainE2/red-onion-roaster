936d8de2f425e1ff9f35542f373c89705a24ce82
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-15 23:54:16 +0600

    json file create and implement data

---
+++ __notes.js
@@ -0,0 +1,36 @@
+/* breakfast img  
+https://i.ibb.co/Vwvdptf/breakfast1.png
+https://i.ibb.co/KWMYPnz/breakfast2.png
+https://i.ibb.co/r4p1D3q/breakfast3.png
+https://i.ibb.co/3st6YrL/breakfast4.png
+https://i.ibb.co/Cz4fSjw/breakfast5.png
+https://i.ibb.co/qB4xvKR/breakfast6.png
+
+    dinner 
+https://i.ibb.co/0tH9t9t/dinner1.png
+https://i.ibb.co/GJRwcRG/dinner2.png
+https://i.ibb.co/dpPDJVn/dinner3.png
+https://i.ibb.co/zV001pH/dinner4.png
+https://i.ibb.co/XWZ7f3c/dinner5.png
+https://i.ibb.co/RhLzVKh/dinner6.png
+
+    lunch 
+https://i.ibb.co/Hn5BVZn/lunch1.png
+https://i.ibb.co/rfx7Vdw/lunch2.png
+https://i.ibb.co/MG8r1Bt/lunch3.png
+https://i.ibb.co/b6YZj0j/lunch4.png
+https://i.ibb.co/D9sCpff/lunch5.png
+https://i.ibb.co/XXSpXwX/lunch6.png
+
+{
+        "id" : "310" ,
+        "name " : ""  ,
+        "nameId" : "" ,
+        "price" : "" ,
+        "title" : ""
+    }
+
+
+
+
+*/

5fdf141077829fc0844c88c2a91712722708e24c
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-16 12:23:53 +0600

    service section complete with style

--- src/components/SingleFood/SingleFood.js
+++ src/components/SingleFood/SingleFood.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './SingleFood.css'
 const SingleFood = (props) => {
-    const {name ,img,price,title,id} = props.food
+    const { name, img, price, title, id } = props.food
     return (
         <div className='col-lg-4 col-md-6 p-2 mb-3'>
-            <div className="cart p-3 m-3 cart-container text-center">
-                <img className='w-75  m-auto  rounded-circle p-1 ' src={img} alt="" />
-                <h5 className='py-2'>{name}</h5>
-                <p className='py-2'>{title.slice(0, 30) + "..."}</p>
-                <h4>${price}</h4>
-                <Link to={id}><button className='btn btn-danger mt-2 w-100'>View Details</button></Link>
+            <div className="cart p-3 m-3  text-center">
+
+                <Link to={id} className='text-decoration-none text-dark'>
+                    <img className='w-75  m-auto  rounded-circle p-1 ' src={img} alt="" />
+                    <h5 className='py-2'>{name}</h5>
+                    <p className='py-2'>{title.slice(0, 30) + "..."}</p>
+                    <h4>${price}</h4>
+                </Link>
             </div>
         </div>
     );
--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -6,21 +6,21 @@ import { GrDeliver } from 'react-icons/gr';
 
 
 const Service = (props) => {
-    const {name,img,title} = props.service
+    const { name, img, title } = props.service
     return (
         <div className='col-lg-4 col-md-6 p-2 mb-3'>
             <div className="cart p-3 m-3 service-container ">
                 <img className='w-100  m-auto  p-1 ' src={img} alt="" />
                 <div className="row ">
-                    <div className="col-md-1 mt-3 ms-2 me-3  fw-bold fs-3">
-                        <div className='service-icon'>
-                        <GrDeliver/>
+                    <div className="col-md-1 mt-2 ms-2 me-3  fw-bold fs-2">
+                        <div className='service-icon ' value={{ color: "blue" }}>
+                            <GrDeliver />
                         </div>
                     </div>
                     <div className="col-md-10">
-                    <h5 className='py-2'>{name}</h5>
-                <p className='py-2'>{title}</p>
-                <Link to='/'><button className='btn btn-link text-decoration-none fs-5 fw-bold ps-0'>See more <BsFillArrowRightCircleFill /> </button></Link>
+                        <h5 className='py-2'>{name}</h5>
+                        <p className='py-2'>{title}</p>
+                        <Link to='/'><button className='btn btn-link text-decoration-none fs-5 fw-bold ps-0'>See more <BsFillArrowRightCircleFill /> </button></Link>
                     </div>
                 </div>
             </div>
